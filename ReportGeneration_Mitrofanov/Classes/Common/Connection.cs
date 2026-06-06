1d0f2648ae6a7d6c09e004239a48e729c6193567
oskar1269 <fahrutdinovoskar4@gmail.com>
2026-06-06 09:57:37 +0500

    Разработанный класс подключения к БД. Митрофанов.

---
+++ ReportGeneration_Mitrofanov/Classes/Common/Connection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ReportGeneration_Mitrofanov.Classes.Common
+{
+    public class Connection
+    {
+        private static string connectionString = "server=localhost;port=3306;database=journal;uid=root;pwd=;";
+
+        public static MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+
+        public static void CloseConnection(MySqlConnection connection)
+        {
+            if (connection != null && connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+
+        public static MySqlDataReader Query(string sql, MySqlConnection connection)
+        {
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            return command.ExecuteReader();
+        }
+    }
+}
